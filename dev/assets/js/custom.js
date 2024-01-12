ed216692448166ec7eef1e50a7a60d7f6ee977db
RostSuslov <suslovros@gmail.com>
2024-01-12 21:21:29 +0200

    feat: creat burger icon custom animation

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,9 @@
-  
+const burger = document.querySelector('.btn-burger');
+const navigation = document.querySelector('.header__navigation');
+
+const toggleMenu =()=> {    
+    burger.classList.toggle('open');
+    navigation.classList.toggle('show');
+}
+
+burger.addEventListener('click', toggleMenu);
